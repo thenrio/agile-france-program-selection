5e1ca94cd9ccef9d3f95f724cc40022b2c4cc471
thierry <thierry.henrio@gmail.com>
2010-04-15 10:50:24 +0200

    change database path

--- lib/agile-france-program-selection.rb
+++ lib/agile-france-program-selection.rb
@@ -3,7 +3,7 @@ require 'configuration'
 require 'model'
 require 'render'
 
-Configuration.new :path => '/Users/thenrio/src/python/xpdayfr/prod.db'
+Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/prod.db'
 render = Renderer.new
 render.render_sessions_with_template(Session.all, 'sessions.html.erb') do |content|
   render.write content
