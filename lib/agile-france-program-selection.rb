2e0cbfd344f9e2cb17302e6eff2ca0fddcbeb4f2
thierry <thierry.henrio@gmail.com>
2010-04-15 12:27:02 +0200

    build one file per category

--- lib/agile-france-program-selection.rb
+++ lib/agile-france-program-selection.rb
@@ -3,8 +3,18 @@ require 'configuration'
 require 'model'
 require 'render'
 
+def render_category(render, category)
+  sessions = Session.all(:category => category)
+  render.render_sessions_with_template(sessions, 'sessions.html.erb') do |content|
+    render.write(content, "#{category}.html")
+  end
+end
+
 Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/prod.db'
 render = Renderer.new
-render.render_sessions_with_template(Session.all, 'sessions.html.erb') do |content|
-  render.write content
+
+Session::Category.all do |category|
+  render_category(render, category)
 end
+
+
