69a5942873f9810655c9868cf8bd0878e69fb4e7
thierry <thierry.henrio@gmail.com>
2010-04-15 16:26:42 +0200

    use dummy hand pager

--- lib/agile-france-program-selection.rb
+++ lib/agile-france-program-selection.rb
@@ -2,28 +2,11 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'configuration'
 require 'model'
 require 'render'
-
-def split(sessions, duration)
-  split = []
-  chunk = nil
-  d = 0
-  sessions.each do |session|
-    d += session.duration
-    if d % duration == 0
-      chunk = nil
-    end
-    if chunk
-      chunk << session
-    else
-      split << (chunk = [])
-    end
-  end
-  split
-end
+require 'pager'
 
 def render_category(render, category)
   sessions = Session.all(:category => category)
-  sessions_per_unit_of_times = split(sessions, 360)
+  sessions_per_unit_of_times = Pager.paginate(sessions, 360)
 
   sessions_per_unit_of_times.each_with_index do |ss, index|
     render.render_sessions_with_template(ss, 'sessions.html.erb') do |content|
