50c75440f2d15f9436040fb58b733565aaada701
thierry <thierry.henrio@gmail.com>
2010-04-19 23:53:30 +0200

    migration does fail few time to get this functionnal, so ... keep it, untested and failing

---
+++ lib/migration/add_scheduled_to_session.rb
@@ -0,0 +1,30 @@
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..'))
+require 'dm-migrations/migration_runner'
+require 'configuration'
+
+DataMapper::Logger.new(STDOUT, :debug)
+DataMapper.logger.debug( "Starting Migration" )
+
+Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/prod.db'
+
+migration 1, :add_scheduled_to_talk_proposal do
+  up do
+    modify_table :talk_proposal do
+      add_column :scheduled, Boolean, :default => false
+    end
+  end
+
+  down do
+    modify_table :talk_proposal do
+      drop_column :scheduled
+    end
+  end
+end
+
+if $0 == __FILE__
+  if $*.first == "down"
+    migrate_down!
+  else
+    migrate_up!
+  end
+end
