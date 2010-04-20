c0a0cbb1366790e7863e0facf3e1e7bdb002a8d6
thierry <thierry.henrio@gmail.com>
2010-04-20 02:24:33 +0200

    nice :) should kick off mailing credentials

--- lib/agile-france-mailer.rb
+++ lib/agile-france-mailer.rb
@@ -3,10 +3,21 @@ require 'configuration'
 require 'model'
 require 'mailer'
 
-Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/prod.db'
+Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/thenrio.db'
 
 # and what about some rake task ?
 # will look better...
 
+options = { :address => "smtpauth.dbmail.com",
+            :domain => "dbmail.com",
+            :port => 25,
+            :user_name => "thierry.henrio@dbmail.com",
+            :password => "vhx224wub",
+            :authentication => :login}
 
+Mail.defaults do
+  delivery_method :smtp, options
+end
+
+Mailer.new.mail_speaker_having_at_least_one_scheduled_session
 
