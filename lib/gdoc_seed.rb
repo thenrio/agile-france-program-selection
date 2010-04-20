c58205233985517979daa1ec0a56d1ea7f42bdb4
thierry <thierry.henrio@gmail.com>
2010-04-20 09:52:54 +0200

    rename seed

--- lib/gdoc_seed.rb
+++ lib/gdoc_seed.rb
@@ -5,11 +5,13 @@ require 'model'
 
 Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/prod.db'
 CSV.foreach(File.join(File.dirname(__FILE__), '../cosel-agile-france-2010.csv')) do |csv|
+  scheduled = csv[0]
   key = csv[1]
   vote = csv[20]
   session = Session.first(:key => key)
   if session
     session.vote = vote
+    session.scheduled = scheduled.casecmp('X') if scheduled
     session.save!
   end
 end
