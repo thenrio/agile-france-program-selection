e7e03af5740356b4672eb6fdd2cdad08420fbfb1
thierry <thierry.henrio@gmail.com>
2010-04-20 10:05:42 +0200

    spec Speaker.scheduled_session

--- spec/speaker_spec.rb
+++ spec/speaker_spec.rb
@@ -1,11 +1,28 @@
 require "spec_helper.rb"
 require "model"
+require 'configuration'
+
 
 describe 'Speaker' do
+  before do
+    Configuration.new.test
+    @speaker = Speaker.new(:firstname => 'John', :lastname => 'Doe', :email => 'john@doe.org')
+    @speaker.save
+    @s1 = Session.new(:title => 'diner', :speaker => @speaker, :scheduled => true)
+    @s1.save
+    @s2 = Session.new(:title => 'pub', :speaker => @speaker)
+    @s2.save
+    @sessions = [@s1, @s2]
+  end
   describe 'full_name' do
     it 'should be John Doe' do
-      speaker = Speaker.new(:firstname => 'John', :lastname => 'Doe')
-      speaker.full_name.should == 'John Doe'
+      @speaker.full_name.should == 'John Doe'
+    end
+  end
+
+  describe 'scheduled_sessions' do
+    it 'should be sessions scoped by scheduled' do
+      @speaker.scheduled_sessions.should == [@s1]    
     end
   end
 end
