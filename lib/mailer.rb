6cf2b05a6887eb7aed3bdc0a7e4ee7770ce252a3
thierry <thierry.henrio@gmail.com>
2010-04-20 10:55:06 +0200

    so bad logs

--- lib/mailer.rb
+++ lib/mailer.rb
@@ -6,8 +6,9 @@ require 'logger'
 
 class Mailer
   @@logger = Logger.new('mailer.log')
-
+  @@mail_logger = Logger.new('mails.log')
   include Renderable
+
   def mail_speaker_having_at_least_one_scheduled_session
     speakers = Speaker.all(:sessions => {:scheduled => true})
     mails = []
@@ -26,7 +27,10 @@ class Mailer
       subject 'vous avez une session retenue au programme de la conférence Agile France'
       body content
     end
-    @@logger.info "sending to #{speaker.email} : #{speaker.scheduled_sessions} are approved"
+    scheduled_sessions = []
+    speaker.scheduled_sessions.each {|session| scheduled_sessions << session.title}
+    @@logger.info "sending to #{speaker.email} : #{scheduled_sessions} are approved"
+    @@mail_logger.info "#{mail} => #{mail.body}"
     mail.deliver!
   end
 end
