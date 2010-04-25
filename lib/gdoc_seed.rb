1d96642c546ecaba995ff9252e599460b4607323
thierry <thierry.henrio@gmail.com>
2010-04-25 23:49:03 +0200

    such a pain in the ass, this runtime dependency on a csv seed file ... getting direct access to gdoc can be done though ... http://github.com/gimite/google-spreadsheet-ruby

--- lib/gdoc_seed.rb
+++ lib/gdoc_seed.rb
@@ -3,16 +3,32 @@ require 'csv'
 require 'configuration'
 require 'model'
 
-Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/prod.db'
+Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/db/prod.db'
+
+DAYS = {1 => '2010/05/31', 2 => '2010/06/01'}
+
+def add_schedule_information(session, day, room, scheduled, time)
+  begin
+    session.scheduled = (scheduled.upcase == 'X')
+    session.room = Room.first(:name => room)
+    session.scheduled_at = DateTime.parse("#{DAYS[Integer(day)]} #{time}")
+  rescue StandardError => e
+    puts "failed to add scheduled information to #{session.inspect}, #{e}"
+  end
+end
+
 CSV.foreach(File.join(File.dirname(__FILE__), '../cosel-agile-france-2010.csv')) do |csv|
   scheduled = csv[0]
   key = csv[1]
-  vote = csv[20]
+  vote = csv[24]
+  room = csv[9]
+  day = csv[10]
+  time = csv[11]
   session = Session.first(:key => key)
   if session
     session.vote = vote
     if scheduled
-      session.scheduled = (scheduled.upcase == 'X')
+      add_schedule_information(session, day, room, scheduled, time)
     end
     session.save!
   end
