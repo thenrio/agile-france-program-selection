9672011c72ae6e5f9710a20e568d7a554ab3f5ff
thierry <thierry.henrio@gmail.com>
2010-04-25 22:10:49 +0200

    save!

--- spec/speaker_spec.rb
+++ spec/speaker_spec.rb
@@ -7,11 +7,11 @@ describe 'Speaker' do
   before do
     Configuration.new.test
     @speaker = Speaker.new(:firstname => 'John', :lastname => 'Doe', :email => 'john@doe.org')
-    @speaker.save
+    @speaker.save!
     @s1 = Session.new(:title => 'diner', :speaker => @speaker, :scheduled => true)
-    @s1.save
+    @s1.save!
     @s2 = Session.new(:title => 'pub', :speaker => @speaker)
-    @s2.save
+    @s2.save!
     @sessions = [@s1, @s2]
   end
   describe 'full_name' do
