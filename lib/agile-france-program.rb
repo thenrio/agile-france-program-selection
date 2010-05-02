7d9144835b471585fa2a9e2c6f577a089b7aaac8
thierry <thierry.henrio@gmail.com>
2010-05-03 00:44:26 +0200

    make some program still horrible layout

--- lib/agile-france-program.rb
+++ lib/agile-france-program.rb
@@ -3,24 +3,16 @@ require 'configuration'
 require 'model'
 require 'renderer'
 
-def render_category(render, category)
-  sessions = Session.all(:category => category, :order => [:vote.desc])
-  sessions_per_unit_of_times = Pager.paginate(sessions, 360)
-
-  sessions_per_unit_of_times.each_with_index do |ss, index|
-    render.render_sessions_with_template(ss, 'sessions-with-your-hands.html.erb') do |content|
-      render.write(content, "#{category}-#{index}.html")
-    end
-  end
-end
-
 Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-program-selection/db/prod.db'
 render = Renderer.new
 
-# and what about some rake task ?
-# will look better...
-Session::Category.all do |category|
-  render_category(render, category)
+
+scheduled_sessions = Session.all(:scheduled_at.not => nil, :order => [:scheduled_at.asc])
+day_one = scheduled_sessions.select {|session| session if session.scheduled_at.day == 31}
+
+render.render_sessions_with_template(day_one, 'program.html.erb') do |content|
+  render.write(content, 'program.html')
 end
+
 
 
