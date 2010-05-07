1c620475374118faca02586cb7f94a3f5da11ecb
thierry <thierry.henrio@gmail.com>
2010-05-07 23:02:22 +0200

    red, do not compile

---
+++ spec/invoice/invoicer_spec.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+require 'configuration'
+require 'rr'
+require 'invoicer'
+
+describe Invoicer do
+  before do
+    @invoicer = Invoicer.new
+  end
+  it 'should have a connector' do
+
+  end
+end
