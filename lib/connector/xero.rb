9cfff9eed0aa07540ac6affe33cc579c6346d2c0
thierry <thierry.henrio@gmail.com>
2010-05-08 16:12:44 +0200

    define xero connector initializer

--- lib/connector/xero.rb
+++ lib/connector/xero.rb
@@ -1,9 +1,16 @@
 require 'connector/base'
+require 'oauth'
+require 'oauth/signature/rsa/sha1'
 
 module Connector
   class Xero < Base
+    def initialize(consumer, secret, options)
+    end
+
     def put_invoice(company, invoiceables)
-      
+    end
+
+    def create_invoice(company, invoiceables)
     end
   end
 end
--- spec/invoice/connector/xero_spec.rb
+++ spec/invoice/connector/xero_spec.rb
@@ -1,10 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+require 'rr'
 require 'connector/xero'
 
 describe Connector::Xero do
   before do
-    @options =
-    @connector = Connector::Xero
+    @options = {}
+    @consumer = 'consumer'
+    @secret = 'secret'
+
+    @connector = Connector::Xero.new(@consumer, @secret, @options)
   end
   describe 'put_invoice' do
     it 'should' do
