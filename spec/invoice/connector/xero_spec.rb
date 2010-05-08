86a4bac4e316a60554f426aea7bfd380fd48e6d1
thierry <thierry.henrio@gmail.com>
2010-05-08 21:21:49 +0200

    enough with oauth collaborator spec reuse of token

--- spec/invoice/connector/xero_spec.rb
+++ spec/invoice/connector/xero_spec.rb
@@ -10,15 +10,39 @@ describe Connector::Xero do
 
     @connector = Connector::Xero.new(@consumer, @secret, @options)
   end
+
   describe 'access_token' do
-    before do
+    # will have to go and see this code once again
+    # there is smell in creating a new 'irritating' dependency on a method
+    # though, value expected is COC
+    #
+    # use plain object 'token' rather than mock!
+    # hence can not make it a fixture for any more functionality
+    #
+    # following fails with rr-0.10.11
+    #
+    #describe 'rr' do
+    #  it 'shouldn't mock! equals self ?' do
+    #    o = mock!
+    #    o.should == o
+    #  end
+    #end
+    #
+    # is it important ?
+    # no
+    it 'should initialize token with appropriate parameters' do
       consumer, @token = 'hello', 'there'
-      mock(OAuth::Consumer).new(@consumer, @secret, @options) {consumer}
-      mock(OAuth::AccessToken).new(consumer, @consumer, @secret) {@token}
+      mock(OAuth::Consumer).new(@consumer, @secret, @options) { consumer }
+      mock(OAuth::AccessToken).new(consumer, @consumer, @secret) { @token }
+      @connector.access_token.should == @token
     end
 
-    it 'should initialize token with appropriate parameters' do
-      @connector.access_token.should == @token
+    it 'should reuse existing token' do
+      token = Object.new
+      @connector.access_token = token
+      @connector.access_token.should equal token
     end
   end
 end
+
+
