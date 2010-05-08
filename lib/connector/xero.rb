7422104530541b46f4a625dcbdbd749b2376f481
thierry <thierry.henrio@gmail.com>
2010-05-08 21:25:10 +0200

    good enough

--- lib/connector/xero.rb
+++ lib/connector/xero.rb
@@ -4,6 +4,7 @@ require 'oauth/signature/rsa/sha1'
 
 module Connector
   class Xero < Base
+    attr_writer :access_token
     def initialize(consumer_key, secret_key, options)
       @consumer_key = consumer_key
       @secret_key = secret_key
@@ -11,6 +12,7 @@ module Connector
     end
 
     def access_token
+      return @access_token if @access_token
       consumer = OAuth::Consumer.new(@consumer_key, @secret_key, @options)
       @access_token = OAuth::AccessToken.new(consumer, @consumer_key, @secret_key)
       @access_token
