cc8858d9d2834d2e70297dfb7d1f2dc1fcdc880e
thierry <thierry.henrio@gmail.com>
2010-05-10 23:11:59 +0200

    repair program-with-your-hands script

--- lib/agile-france-program-with-your-hands.rb
+++ lib/agile-france-program-with-your-hands.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'configuration'
-require 'model'
+require 'model/program'
 require 'renderer'
 require 'pager'
 
@@ -9,7 +9,7 @@ def render_category(render, category)
   sessions_per_unit_of_times = Pager.paginate(sessions, 360)
 
   sessions_per_unit_of_times.each_with_index do |ss, index|
-    render.render({:sessions => ss}, 'sessions-with-your-hands.html.erb') do |content|
+    render.render('sessions-with-your-hands.html.erb', :sessions => ss) do |content|
       render.write(content, "#{category}-#{index}.html")
     end
   end
