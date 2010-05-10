76afacaea0c746798c326e70ee77354d595babca
thierry <thierry.henrio@gmail.com>
2010-05-10 23:29:58 +0200

    minor renaming

--- lib/agile-france-program-with-your-hands.rb
+++ lib/agile-france-program-with-your-hands.rb
@@ -4,24 +4,30 @@ require 'model/program'
 require 'renderer'
 require 'pager'
 
-def render_category(render, category)
+def render_category(renderer, category)
   sessions = Session.all(:category => category, :order => [:vote.desc])
   sessions_per_unit_of_times = Pager.paginate(sessions, 360)
 
   sessions_per_unit_of_times.each_with_index do |ss, index|
-    render.render('sessions-with-your-hands.html.erb', :sessions => ss) do |content|
-      render.write(content, "#{category}-#{index}.html")
+    renderer.render('sessions-with-your-hands.html.erb', :sessions => ss) do |content|
+      renderer.write(content, "#{category}-#{index}.html")
     end
   end
 end
 
+def copy_css_to_output_directory
+  include Renderable
+  mkdir_p output_dir
+  cp_r File.join(template_dir, 'css'), output_dir
+end
+
 Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-database/prod.db'
-render = Renderer::Erb.new
+renderer = Renderer::Erb.new
+
+copy_css_to_output_directory()
 
 # and what about some rake task ?
 # will look better...
 Session::Category.all do |category|
-  render_category(render, category)
+  render_category(renderer, category)
 end
-
-
