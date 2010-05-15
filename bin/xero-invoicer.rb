0dea05dbc4a4d4b2cda6161a4cc1d092dad3efca
thierry <thierry.henrio@gmail.com>
2010-05-16 01:15:16 +0200

    format code add wip invoices

--- bin/xero-invoicer.rb
+++ bin/xero-invoicer.rb
@@ -5,18 +5,19 @@ require 'configuration'
 require 'model/company'
 
 options = {:site => 'https://api.xero.com',
-:request_token_path => "/oauth/RequestToken",
-:access_token_path  => "/oauth/AccessToken",
-:authorize_path     => "/oauth/Authorize",
-:signature_method => 'RSA-SHA1',
-:private_key_file => '/Users/thenrio/src/ruby/spike-on-xero/keys/xero.rsa'}
+           :request_token_path => "/oauth/RequestToken",
+           :access_token_path  => "/oauth/AccessToken",
+           :authorize_path     => "/oauth/Authorize",
+           :signature_method => 'RSA-SHA1',
+           :private_key_file => '/Users/thenrio/src/ruby/spike-on-xero/keys/xero.rsa'}
 consumer_key = 'NTA0YZDJZTM0M2JHNDQ0MMJHY2NLMT'
 secret_key = 'XHHWNGJGRUDMXQKVBQIZEBGG2ROFRF'
 connector = Connector::Xero.new(consumer_key, secret_key, options)
 
-
 invoicer = Invoicer.new(connector)
 Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-database/prod.db'
+invoices = []
 Company.all.each do |company|
-  invoicer.invoice_company company
+  invoices << invoicer.invoice_company(company)
 end
+
