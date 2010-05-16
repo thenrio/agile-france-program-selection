bd800b8dc696b3953d0023cff88e32edd6cfbb75
thierry <thierry.henrio@gmail.com>
2010-05-17 00:11:25 +0200

    use keys located in ./keys

--- features/step_definitions/xero_invoice_steps.rb
+++ features/step_definitions/xero_invoice_steps.rb
@@ -11,7 +11,7 @@ Given 'a XeroInvoicer using sandbox xero account' do
              :access_token_path  => "/oauth/AccessToken",
              :authorize_path     => "/oauth/Authorize",
              :signature_method => 'RSA-SHA1',
-             :private_key_file => '/Users/thenrio/src/ruby/spike-on-xero/keys/xero.rsa'}
+             :private_key_file => File.join(File.dirname(__FILE__), '../../keys/xero.rsa')}
   consumer_key = 'NTA0YZDJZTM0M2JHNDQ0MMJHY2NLMT'
   secret_key = 'XHHWNGJGRUDMXQKVBQIZEBGG2ROFRF'
   @connector = Connector::Xero.new(consumer_key, secret_key, options)
