f9bf5b9461bceabca4c871ca30a7a9d31a24502c
thierry <thierry.henrio@gmail.com>
2010-05-17 23:13:46 +0200

    require less

--- db/seeds/csv.rb
+++ db/seeds/csv.rb
@@ -1,6 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../config/boot')
 require 'csv'
-require 'configuration'
 require 'model/program'
 
 DAYS = {1 => '2010/05/31', 2 => '2010/06/01'}
--- db/seeds/rooms.rb
+++ db/seeds/rooms.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../config/boot')
-
 require 'model/program'
+
 ('A'..'G').each do |name|
   Room.create(:name => name) unless Room.first(:name => name)
 end
