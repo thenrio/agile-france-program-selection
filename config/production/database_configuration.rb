5acdb75921ce24e111db9148daaf69363f114da4
thierry <thierry.henrio@gmail.com>
2010-05-18 01:45:18 +0200

    remove trash files

--- config/production/database_configuration.rb
+++
@@ -1,3 +0,0 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../lib'))
-require 'configuration'
-$database_configuration ||= Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-database/prod.db'
--- config/production/xero_configuration.rb
+++
@@ -1,8 +0,0 @@
-$xero_options = {:site => 'https://api.xero.com',
-           :request_token_path => "/oauth/RequestToken",
-           :access_token_path  => "/oauth/AccessToken",
-           :authorize_path     => "/oauth/Authorize",
-           :signature_method => 'RSA-SHA1',
-           :private_key_file => File.join(File.dirname(__FILE__), '../../keys/xero.rsa')}
-$xero_consumer_key = 'Y2MYZTEXMZG3MDRHNGFIZDK1MJZMYZ'
-$xero_secret_key = 'V1XDBBKOKVUWQ90LXTGYRVTNNJHWVI'
