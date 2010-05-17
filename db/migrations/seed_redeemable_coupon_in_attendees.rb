da3ca3f7003007e2d47af918d136244004c2fac6
thierry <thierry.henrio@gmail.com>
2010-05-17 11:47:07 +0200

    reorganize migrations and seeds to separate folder db

--- db/migrations/seed_redeemable_coupon_in_attendees.rb
+++
@@ -1,6 +0,0 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '../../lib'))
-require 'dm-migrations/migration_runner'
-
-require 'configuration'
-Configuration.new :path => '/Users/thenrio/src/ruby/agile-france-database/prod.db'
-
