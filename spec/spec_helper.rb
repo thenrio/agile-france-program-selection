3aa22f366b8d5578a1f9c49675f105dbe72a301c
thierry <thierry.henrio@gmail.com>
2010-05-20 11:34:43 +0200

    empty database method

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,3 +10,8 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'config'))
 ENV['AGILE_FRANCE_ENV'] = 'test'  
 require 'boot'
+
+def empty_database
+  require 'configuration'
+  Configuration.new.test
+end
