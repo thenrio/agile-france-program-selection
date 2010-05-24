3fa47c7a2688ca33aabdfc664eb2bad6d79a1647
thierry <thierry.henrio@gmail.com>
2010-05-24 11:03:33 +0200

    reedem with SPEAKER for a speaker

--- db/seeds/redeemable_coupon.rb
+++ db/seeds/redeemable_coupon.rb
@@ -6,12 +6,12 @@ require 'model/invoice'
 require 'ruby-debug'
 
 def redeem(attendee)
-  attendee.update(:redeemable_coupon => 'ORGANIZATION')
+  attendee.update(:redeemable_coupon => 'SPEAKER')
   puts "#{attendee.inspect} gain redeemable coupon"
 end
 
 def agile_france
-  @agile_france ||= Company.get(:name => 'agile-france')
+  @agile_france ||= Company.get(:email => 'orga@conf.agile-france.org')
   unless @agile_france
     filler = 'filler'
     attributes = {:name => 'agile-france',
@@ -28,8 +28,8 @@ Speaker.scheduled.each do |speaker|
   if attendee
     redeem(attendee)
   else
-    attributes = {:firstname => speaker.firstname, :lastname => speaker.lastname, :email => speaker.email, 
-                  :company => agile_france, :redeemable_coupon => 'ORGANIZATION', :lunch => 1}
+    attributes = {:firstname => speaker.firstname, :lastname => speaker.lastname, :email => speaker.email,
+                  :company => agile_france, :redeemable_coupon => 'SPEAKER', :lunch => 1}
     attendee = Attendee.create(attributes)
     puts "#{speaker.inspect} attends as #{attendee.inspect}"
   end
